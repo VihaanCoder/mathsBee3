8ce544a7572f9ef72f7ed10ce3466421b2785ed7
VihaanCoder <74820062+VihaanCoder@users.noreply.github.com>
2021-02-14 16:18:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function  addUser(){
+function addUser() {
+    playerRedName = document.getElementById("playerRedName").value;
+    playerBlueName = document.getElementById("playerBlueName").value;
+
+    localStorage.setItem("playerRedName", palyerRedName);
+    localStorage.setItem("playerBlueName", palyerBlueName);
+
+    window.location = "game_page.html";
     
 }
